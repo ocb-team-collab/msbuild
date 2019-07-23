bf7204966ebb8ac0add3f1d2331e4f81777bce3e
Ken Breiding <kenbreid@microsoft.com>
2019-07-23 13:33:20 -0700

    Inital implementation of metabuild only tasks

---
+++ src/Framework/ITaskHybrid.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Build.Framework
+{
+    /// <summary>
+    /// This interface defines a static "task" in the build system that can execute while generating a static graph of build actions.  
+    /// </summary>
+    public interface ITaskHybrid : ITask
+    {
+        /// <summary>
+        /// This method is called by the build engine to begin static task execution for tasks that run both during graph creation and during the build.
+        /// A task uses the return value to indicate whether it was successful. If a task throws an exception out of this method, the engine will
+        /// automatically assume that the task has failed.
+        /// </summary>
+        /// <returns>true, if successful</returns>
+        bool ExecuteStatic();
+    }
+}
